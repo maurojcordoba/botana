e286ada565d93db205448b93811fba5667b41fce
maurojcordoba <mauro.j.cordoba@gmail.com>
2021-10-19 18:36:59 -0300

    Segundo commit

--- botana.py
+++ botana.py
@@ -70,7 +70,12 @@ def main() -> None:
     #dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
     # Start the Bot
-    updater.start_polling()
+    #updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(config.port),
+                          url_path=config.token)
+                          
+    updater.bot.setWebhook('https://botanabot.herokuapp.com/' + config.token)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 token = '1888619574:AAH_6DMxt5tFww4Y2vj_9fKbhYrjYcFJ4tA'
+port = 5000
