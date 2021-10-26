c25d52a7b472a0d8928dceb5787bb4a983845764
maurojcordoba <mauro.j.cordoba@gmail.com>
2021-10-26 11:48:38 -0300

    Elimino archivo config.py, cambio path de imagenes

--- botana.py
+++ botana.py
@@ -16,7 +16,6 @@ bot.
 """
 
 import logging
-import config
 import os
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
--- config.py
+++
@@ -1,1 +0,0 @@
-token = '1888619574:AAH_6DMxt5tFww4Y2vj_9fKbhYrjYcFJ4tA'
